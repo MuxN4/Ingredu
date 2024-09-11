365a57e4f0a82a20617df501831a6a1de43e07f4
MuxN4 <stev3npy@gmail.com>
2024-09-11 11:32:23 +0300

    Completion of the Mobile Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <>
      <DesktopSidebar/>
-     {/* <MobileSidebar/> */}
+     <MobileSidebar/>
     </>
   )
 }
@@ -32,6 +32,20 @@ const DesktopSidebar = () => {
           </Link>
         </ul>
       </div>
+    </div>
+  )
+}
+
+const MobileSidebar = () => {
+  return (
+    <div className='flex justify-center gap-10 border-t fixed w-full
+    bottom-0 left-0 bg-slate-100 z-10 p-2 sm:hidden'>
+      <Link to={"/"} >
+        <Home size={"24"} className='cursor-pointer'/>
+      </Link>
+      <Link to={"/favorites"} >
+        <Heart size={"24"} className='cursor-pointer'/>
+      </Link>
     </div>
   )
 }
