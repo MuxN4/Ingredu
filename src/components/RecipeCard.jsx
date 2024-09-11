c2a7da3cc904fbd86bc621142279b290afd2de36
MuxN4 <stev3npy@gmail.com>
2024-09-11 16:19:58 +0300

    Demo image

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -5,7 +5,7 @@ const RecipeCard = () => {
   return (
     <div className='flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative'>
         <a href="#" className='relative h-32'>
-            <img src="" alt="recipe image" className='rounded-md w-full h-full object-cover 
+            <img src="2.jpg" alt="recipe image" className='rounded-md w-full h-full object-cover 
             cursor-pointer' />
             <div className='absolute bottom-2 left-2 bg-white rounded-full p-1 
             cursor-pointer flex items-center gap-1 text-sm'>
